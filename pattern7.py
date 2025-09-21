f9645ab605114783134da04ff38f2ea6ef09f4c3
Rakhi-Nishad <rakhi.gla_bca24@gla.ac.in>
2025-09-21 12:18:31 +0530

    Add files via upload

---
+++ pattern7.py
@@ -0,0 +1,14 @@
+n = 5
+for i in range(n):
+    if i == 0 or i == n - 1:
+        print("* " * 5)
+    else:
+        print("* *")
+
+
+
+# * * * * * 
+# * * 
+# * * 
+# * * 
+# * * * * *
---
+++ pattern8.py
@@ -0,0 +1,14 @@
+n = 5
+for i in range(n):
+    if i == 2:
+        print("*")
+    else:
+        print("* *")
+
+
+
+# * * 
+# * * 
+# * 
+# * * 
+# * * 
